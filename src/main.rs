668c696244b18ccc207869da8ce42b13020ac1b5
Nick Wang <yw12me@gmail.com>
2024-07-11 03:40:20 -0400

    first commit to my repo

--- src/main.rs
+++ src/main.rs
@@ -6,7 +6,8 @@ fn main() {
     println!("Logs from your program will appear here!");
 
     // Uncomment this block to pass the first stage
-    //
+    // 6379 is the default port that Redis uses.
+
     let listener = TcpListener::bind("127.0.0.1:6379").unwrap();
     
     for stream in listener.incoming() {
